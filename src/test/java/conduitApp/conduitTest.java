bf282c977d606e1c6be7fe03217e9497889eb452
“atulkr777” <atul.singh@ucdconnect.ie>
2021-06-10 10:45:57 +0100

    initial commit

---
+++ src/test/java/conduitApp/conduitTest.java
@@ -0,0 +1,17 @@
+package conduitApp;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ConduitTest {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:conduitApp").tags("").parallel(1);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+
+
+}
